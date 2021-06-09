e3370c9a767873dbdc3b93d0d4895d18fc4844c2
Robert Freeman <robbie.a.freeman@gmail.com>
2021-06-09 10:54:23 -0500

    Got Spring endpoint of /tectonicMap working

--- src/main/java/edu/ding/map/TectonicCellNetwork.java
+++ src/main/java/edu/ding/map/TectonicCellNetwork.java
@@ -1,30 +1,14 @@
 package edu.ding.map;
 
 import edu.ding.eng.Location;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServletResponse;
-import javax.swing.*;
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.*;
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.concurrent.ConcurrentHashMap;
 
-@RestController
-@EnableAutoConfiguration
+
 public class TectonicCellNetwork implements Drawable {
 
     private ConcurrentHashMap<BigDecimal, ConcurrentHashMap<BigDecimal, Vertex>> allVertices; // first col, then row
@@ -265,48 +249,7 @@ public class TectonicCellNetwork implements Drawable {
         }
     }
 
-    @RequestMapping(value = "/tectonicMap", method = RequestMethod.GET)
-    public void home(HttpServletResponse response) throws IOException {
-        int[] worldSize = new int[] {500, 500};
-        TectonicCellNetwork n = new TectonicCellNetwork(new BigDecimal(2), new BigDecimal(2));
-        n.distributeCells(100);
-        //System.out.println(n.getAllVertices().size());
-        //System.out.println(n.getAllHalfEdges().size());
-        //n.getAllHalfEdges().forEach((i, x) -> System.out.println(x.toString()));
-        /*
-        System.out.println(n.getNominalHalfEdges().get(0).getVertex());
-        System.out.println(n.getNominalHalfEdges().get(0).getFaceId());
-        System.out.println(n.getNominalHalfEdges().get(0).getNextEdge());
-        System.out.println(n.getNominalHalfEdges().get(0).getId());
-        System.out.println(n.getNominalHalfEdges().get(0).getOppositeEdge()); */
 
-        // draw out the network
-        JFrame f = new JFrame();
-        TestPanel p = new TestPanel(n);
-        f.add(p);
-//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        f.setSize(700, 700);
-//        f.setVisible(true);
-
-        BufferedImage bi = new BufferedImage(f.getWidth(), f.getHeight(), BufferedImage.TYPE_INT_RGB);
-        Graphics g = bi.createGraphics();
-        f.print(g);
-        f.dispose();
-        ByteArrayOutputStream s = new ByteArrayOutputStream();
-        try {
-            ImageIO.write(bi, "png", s);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        ByteArrayInputStream in = new ByteArrayInputStream(s.toByteArray());
-        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-        IOUtils.copy(in, response.getOutputStream());
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(TectonicCellNetwork.class, args);
-    }
 
     // Assigns TectonicPlate pointers to their proper Location objects
     public void assignParentPlates(Location[] locs) {
