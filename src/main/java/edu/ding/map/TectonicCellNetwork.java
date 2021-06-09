4208381138d02945a77985d1e48c2a7ff499f56e
robbie-a-freeman <robbie.a.freeman@gmail.com>
2021-06-09 08:49:07 -0500

    Got spring boot to execute via maven

--- src/main/java/edu/ding/map/TectonicCellNetwork.java
+++ src/main/java/edu/ding/map/TectonicCellNetwork.java
@@ -1,10 +1,21 @@
 package edu.ding.map;
 
 import edu.ding.eng.Location;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
 import javax.swing.*;
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.security.SecureRandom;
@@ -12,6 +23,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.concurrent.ConcurrentHashMap;
 
+@RestController
+@EnableAutoConfiguration
 public class TectonicCellNetwork implements Drawable {
 
     private ConcurrentHashMap<BigDecimal, ConcurrentHashMap<BigDecimal, Vertex>> allVertices; // first col, then row
@@ -252,7 +265,8 @@ public class TectonicCellNetwork implements Drawable {
         }
     }
 
-    public static void main(String[] args) {
+    @RequestMapping(value = "/tectonicMap", method = RequestMethod.GET)
+    public void home(HttpServletResponse response) throws IOException {
         int[] worldSize = new int[] {500, 500};
         TectonicCellNetwork n = new TectonicCellNetwork(new BigDecimal(2), new BigDecimal(2));
         n.distributeCells(100);
@@ -270,10 +284,28 @@ public class TectonicCellNetwork implements Drawable {
         JFrame f = new JFrame();
         TestPanel p = new TestPanel(n);
         f.add(p);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(700, 700);
-        f.setVisible(true);
+//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        f.setSize(700, 700);
+//        f.setVisible(true);
 
+        BufferedImage bi = new BufferedImage(f.getWidth(), f.getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics g = bi.createGraphics();
+        f.print(g);
+        f.dispose();
+        ByteArrayOutputStream s = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(bi, "png", s);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        ByteArrayInputStream in = new ByteArrayInputStream(s.toByteArray());
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        IOUtils.copy(in, response.getOutputStream());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TectonicCellNetwork.class, args);
     }
 
     // Assigns TectonicPlate pointers to their proper Location objects
